5051b59dec48da36dd2076fd77ac5a8841a04ad3
Ilya Zavadskiy <ilya.zavadskij@gmail.com>
2020-06-07 21:21:42 +0300

    Add LICENSE

---
+++ peer2peer/src/main/java/com/spbsu/grpc/Application.java
@@ -0,0 +1,10 @@
+package com.spbsu.grpc;
+
+
+import com.spbsu.grpc.client.ChatClient;
+
+public class Application {
+    public static void main(String[] args) {
+        ChatClient.main(args);
+    }
+}
